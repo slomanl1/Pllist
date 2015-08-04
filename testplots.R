f5c42a1345d5ff9c5c3438ad79e08f85dd215317
slomanl1 <slomanl75@gmail.com>
2015-08-04 15:18:46 -0600

    removed return and changed to !tpexist

--- testplots.R
+++ testplots.R
@@ -7,44 +7,46 @@ options(guiToolkit = "RGtk2")                     # avoid question if more than 
 #   galert("Hello world", parent=h$obj)
 # })
 testplots = function(fnames) {
-  if(.GlobalEnv$tpexist)
-    return
-  .GlobalEnv$avail = FALSE
-  w <- gwindow(paste(liner,"Choose One or More Files\n"),width = 800,parent = c(0,0))
-  gp <- ggroup(horizontal = FALSE, container = w)
-  tab <- gtable(
-    fnames, container = gp, expand = TRUE,multiple = TRUE,
-    handler = function(h,...) {
-      print(svalue(h$obj))
-      .GlobalEnv$ssv = svalue(h$obj)
-      .GlobalEnv$avail = TRUE
-    }
-  )
-  addHandlerRightclick(
-    tab, handler = function(h,...) {
-      if (length(svalue(h$obj)) > 0) {
-        msgg = an[ttl][which(grepl(basename(svalue(h$obj)),an[ttl],fixed = TRUE))]
-        msgg = substr(msgg,10,255)
-        msgBox(msgg)
+  if (!.GlobalEnv$tpexist) {
+    .GlobalEnv$avail = FALSE
+    w <-
+      gwindow(
+        paste(liner,"Choose One or More Files\n"),width = 800,parent = c(0,0)
+      )
+    gp <- ggroup(horizontal = FALSE, container = w)
+    tab <- gtable(
+      fnames, container = gp, expand = TRUE,multiple = TRUE,
+      handler = function(h,...) {
+        print(svalue(h$obj))
+        .GlobalEnv$ssv = svalue(h$obj)
+        .GlobalEnv$avail = TRUE
       }
-    }
-  )
-  
-  bg <- ggroup(container = gp)
-  addSpring(bg)
-  gbutton(
-    "dismiss", container = bg, handler = function(h,...) {
-      .GlobalEnv$tpexist <- FALSE
-      .GlobalEnv$avail = TRUE
-      dispose(w)
-    }
-  )
-  addHandlerDestroy(
-    tab, handler = function(h,...) {
-      .GlobalEnv$ssv = NULL
-      .GlobalEnv$avail = TRUE
-      .GlobalEnv$tpexist <- FALSE
-    }
-  )
-  .GlobalEnv$tpexist <- TRUE
+    )
+    addHandlerRightclick(
+      tab, handler = function(h,...) {
+        if (length(svalue(h$obj)) > 0) {
+          msgg = an[ttl][which(grepl(basename(svalue(h$obj)),an[ttl],fixed = TRUE))]
+          msgg = substr(msgg,10,255)
+          msgBox(msgg)
+        }
+      }
+    )
+    
+    bg <- ggroup(container = gp)
+    addSpring(bg)
+    gbutton("dismiss", container = bg, handler = function(h,...) {
+        .GlobalEnv$tpexist <- FALSE
+        .GlobalEnv$avail = TRUE
+        dispose(w)
+      }
+    )
+    addHandlerDestroy(
+      tab, handler = function(h,...) {
+        .GlobalEnv$ssv = NULL
+        .GlobalEnv$avail = TRUE
+        .GlobalEnv$tpexist <- FALSE
+      }
+    )
+    .GlobalEnv$tpexist <- TRUE
+  }
 }
