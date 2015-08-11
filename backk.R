344b4a8d4d70642810d76ffb614275352fd42c63
slomanl1 <slomanl75@gmail.com>
2015-08-11 11:29:28 -0700

    Initial version New backup script for split C/D drive PNMTALL backup to E:

---
+++ backk.R
@@ -0,0 +1,24 @@
+  shell('dir D:\\ /S/B > zz.txt')
+  shell('dir c:\\PNMTALL /S/B >> zz.txt')
+  shell('dir E:\\ /S/B > yy.txt')
+  zz = readLines('zz.txt')
+  yy = readLines('yy.txt')
+  unlink('zz.txt')
+  unlink('yy.txt')
+  copyl = zz[!(substr(zz,2,100) %in% substr(yy,2,100))]
+  reml = yy[!(substr(yy,2,100) %in% substr(zz,2,100))]
+  unlink(reml,recursive = TRUE)
+  ccdirs = sub('D:','E:',na.omit(copyl[file.info(copyl)[,'isdir']]))
+  if (len(ccdirs) > 0)
+    for (i in 1:len(ccdirs)) {
+      dir.create(ccdirs[i])
+      print(paste(ccdirs[i],'created'))
+    }
+  lnc = len(copyl)
+  if (lnc > 0)
+    for (i in 1:lnc) {
+      print(paste('Copying',copyl[i],lnc - i))
+      file.copy(copyl[i],sub('D:','E:',copyl[i]))
+    }
+  shell('xcopy D: E: /S/D/Y/J')
+  print('Done')
