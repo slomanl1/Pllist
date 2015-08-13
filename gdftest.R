1c85e9e3444308e767af675bd24c8eb525f2b71e
slomanl1 <slomanl75@gmail.com>
2015-08-13 16:39:49 -0700

    Initial version, just trying some ops on widgets

---
+++ gdftest.R
@@ -0,0 +1,10 @@
+# obj <- gdf(mtcars, container=gwindow("mtcars"), do.subset=TRUE)
+# xx=obj[,]
+while (!avail & !renamed)
+  tab=gdf(gdframe, container=gwindow("FileSearch",width=1900))
+
+
+EOFN = 'Comment|Title|Sub Title|File Path|Ingredients|Album|File Name|Tracks'
+ttls = unlist(regexpr(EOFN,pnoln))
+testplots(fnames)
+while (!avail & !renamed) {} # testplots returns ssv global
