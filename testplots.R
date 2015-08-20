a938f2e2aea7caef9305da039e13a07d9a9be79f
slomanl1 <slomanl75@gmail.com>
2015-08-20 00:24:42 -0700

    minor, removed extra comments

--- testplots.R
+++ testplots.R
@@ -94,16 +94,4 @@ if (!.GlobalEnv$tpexist) {
 }else
   visible(w)=TRUE
 
-#}
 
-pathPrep <- function(path = "clipboard") {
-  y <- if (path == "clipboard") {
-    readClipboard()
-  } else {
-    cat("Please enter the path:\n\n")
-    readline()
-  }
-  x <- chartr("\\", "/", y)
-  writeClipboard(x)
-  return(x)
-}
