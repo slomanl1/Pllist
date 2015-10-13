73ae5da8676fe5297d7e5fd3c4c235729bca1bfb
slomanl1 <slomanl75@gmail.com>
2015-10-12 23:53:09 -0600

    Added enable/disable to prevent premature abort, fixed destroy flagg

--- selecttt.R
+++ selecttt.R
@@ -12,6 +12,7 @@ xw <- gwindow("Delete sfname and rebuild?",height = 20)
 gp <- ggroup(container=xw)
 
 cb <- gcombobox(flavors, editable=TRUE, container=gp, handler=f)
+enabled(cb)=FALSE
 addHandlerDestroy(xw, handler = function(h,...) {
   .GlobalEnv$destroyed=.GlobalEnv$flagg
   .GlobalEnv$avail=TRUE
@@ -19,14 +20,17 @@ addHandlerDestroy(xw, handler = function(h,...) {
 cntt=7
 delay500()
 shell('nircmd win activate title "Delete sfname and rebuild?"')
+enabled(cb)=TRUE
 focus(cb)=TRUE
 while(!avail)
 {
   svalue(xw)=paste("Delete sfname and rebuild?",cntt)
   delay500()
   cntt=cntt-1
-  if(cntt==0)
+  if(cntt==0){
+    flagg=FALSE
     break
+    }
 }
 if(isExtant(xw))
   dispose(xw)
