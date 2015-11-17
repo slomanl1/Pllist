eb5dff19c34c894f3c07ecd1f93d6419a2f1b8bc
slomanl1 <slomanl75@gmail.com>
2015-11-17 10:15:39 -0800

    Added logic to kill ffmpeg when deleting out.mp4 if neede

--- reduceallmovies.R
+++ reduceallmovies.R
@@ -1,29 +1,54 @@
 cd('~/')
 cls=NULL
-cls=c(dir('D:/PNMTALL',recursive = TRUE,full.names = TRUE))
-cls=c(cls,dir('c:/PNMTALL',recursive = TRUE,full.names = TRUE))
+#cls=c(cls,dir('D:/PNMTALL',recursive = TRUE,full.names = TRUE))
+#cls=c(cls,dir('c:/PNMTALL',recursive = TRUE,full.names = TRUE))
 cls=c(cls,dir('c:/my videos/rpdnclips',recursive = TRUE,full.names = TRUE))
-cls=c(cls,dir('c:/Users/LarrySloman/Downloads',recursive = TRUE,full.names = TRUE))
+#cls=c(cls,dir('c:/Users/LarrySloman/Downloads',recursive = TRUE,full.names = TRUE))
+fna=cls # all files (including _New's)
 cls=cls[which(!grepl('_New',cls))]
-dfa=data.frame(cls,sz=file.size(cls))
-dfa=dfa[order(dfa$sz,decreasing=TRUE),]
-writeLines('','~/msgis.txt')
-for( fn in dfa$cls)
-{
-  if(file.exists(fn) & file.size(fn)==dfa[which(dfa$cls==fn),'sz']){
+cla=fna[which(grepl('_New',fna))]
+dfn=data.frame(cla)
+dfn$cls=sub('_New','',dfn$cla)
+dfn$sz=file.size(dfn$cls)
+dfn=dfn[order(dfn$sz),]
+dfn=dfn[!is.na(dfn$sz),]
+
+nfns=paste(file_path_sans_ext(cls),'_New.',file_ext(cls),sep='')
+dfa=data.frame(cls,sz=file.size(cls),nfns)
+dfa=subset(dfa,!file.exists(as.character(dfa$nfns))) # remove already converted to _New
+#dfa=dfa[order(dfa$sz,decreasing=TRUE),]
+#dfa=dfa[dfa$sz>tail(dfn,1)$sz,] # remove already processed files(to get rid of the <100length results already determeind)
+dfa=dfa[order(dfa$sz,decreasing=FALSE),] # for clips #***************
+
+if(!file.exists('~/msgis.txt')){
+  writeLines('','~/msgis.txt')
+  }
+for(fn in dfa$cls)
+{ 
+  nfn=paste(file_path_sans_ext(fn),'_New.',file_ext(fn),sep='')
+  print(paste(fn,file.size(fn),'nfn-',nfn))
+  if(file.exists(fn) & !file.exists(nfn) & file.size(fn)==dfa[which(dfa$cls==fn),'sz']){
     if(file.exists('~/out.mp4')){
-      file.remove('~/out.mp4')
+      shell('tasklist > tl.txt')
+      tl=readLines('tl.txt')
+      if(any(grepl('ffmpeg.exe',tl,fixed=TRUE)))
+        shell('taskkill /F /IM  ffmpeg.exe')
+      if(!file.remove('~/out.mp4')){
+        stop('Cannot Remove out.mp4, kill ffmpeg.exe')}
     }
-    print(fn)
+
     msgi=shell(paste('c:/users/LarrySloman/converth265.bat "',
                 fn,'" c:/users/LarrySloman/Documents/out.mp4',sep=''),translate = TRUE,intern=TRUE)
     print(tail(msgi))
-    nfn=paste(file_path_sans_ext(fn),'_New.',file_ext(fn),sep='')
-    file.copy('~/out.mp4',nfn)
+    if(file.size('~/out.mp4')>100){
+      file.copy('~/out.mp4',nfn)
+      print(paste(fn,file.size(fn)))
+      print(paste(nfn,file.size(nfn)))
+    }
+    writeLines(msgi,'~/temp.txt')
+    msgi=readLines('~/temp.txt') # convert CR to CRLF
     writeLines(msgi,'~/temp.txt')
     file.append('~/msgis.txt','~/temp.txt')
-
-    print(nfn)
   }
 }
 
