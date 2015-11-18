2d024f9e0f015cd320236ba5f81831e4cf3fc2d0
slomanl1 <slomanl75@gmail.com>
2015-11-17 18:29:17 -0800

    Intermediate version, still no work in context menu

--- trimmer.R
+++ trimmer.R
@@ -1,40 +1,64 @@
+EnterStartStop = function(x="Enter Start Time (secs) or (mm:ss)\n"){
+  .GlobalEnv$ss=NULL
+  while(TRUE){
+    ginput(x, icon="question", handler = function(h,...) .GlobalEnv$ss=h$input)
+    startt= .GlobalEnv$ss   
+    if(len(startt)>0){
+      if(!is.na(as.integer(startt))){
+        break # good integer
+      }else{
+        cpos=regexpr(':',startt)
+        if(cpos>0){
+          f1=as.integer(substr(startt,1,cpos-1))
+          f2=as.integer(substr(startt,cpos+1,nchar(startt)))
+          if (f1>=0 & f1<60 & f2>=0 & f2<60){
+            break # good mm:ss
+          }
+        }
+        
+      }
+    }else{
+      break # bad integer
+    }
+  }
+  return(startt)
+}
+StartMyGUI <- function(handler=function(h,...) {
+  dispose(h$obj)
+}) {
+  startt=EnterStartStop()
+  print(len(startt))
+  if(len(startt)>0){
+    endtt=EnterStartStop("Enter Time Duration (secs) or (mm:ss)\n")
+    unlink('~/temppt.mp4')
+    file.rename(svt,'~/temppt.mp4')
+    svtt1=gsub(' ','',svt) # remove spaces for ffmpeg (does not accept " in filename's)
+    svtt='c:/RealPlayerDownloads/trimmed.mp4'
+    entf=FALSE
+    if(len(endtt)==0){
+      entf=TRUE
+      endtt=10000
+    }
+    cmdd=paste('shell("ffmpeg.exe -ss',startt,' -i c:/users/LarrySloman/Documents/temppt.mp4 -t',endtt,'-c:v copy -c:a copy',svtt,'",mustWork=NA,translate=TRUE)')
+    print(cmdd)
+    eval(parse(text=cmdd))
+    if(entf){
+      file.rename(svtt,svt) # replace svt has trimmed with start to end
+    }else{
+      file.rename('~/temppt.mp4',svt)  # put back original, trimmed.mp4 has trimmed
+    }
+  }
+  return()
+} 
+options("guiToolkit"="RGtk2")
+library(RGtk2)
+library(gWidgets)
+library(gWidgetsRGtk2)
 args <- commandArgs(TRUE)
 save(args,file='~/args.RData')
-scriptStatsRemoveAll <- "~/Revolution/Stats/RemoveAllExceptFuncs.R"
-source(scriptStatsRemoveAll) #clear bones
-load('~/args.RData')
-print(args)
-source('~/pllist/pllist.git/EnterStartStop.R')
-cd('~/')
-#svt=normalizePath(file.choose(),winslash = '/')
 svt=args[1]
-load('~/headfoot.RData')
-writeLines(as.character(c(header,paste('<media src="',svt,'"/>'),footer),sep = ''),'~/svt.wpl')
-#shell("wmplayer c:\\Users\\LarrySloman\\Documents\\svt.wpl")
-unlink('~/svt.wpl')
-startt=NULL
-print(svt)
-
-startt=EnterStartStop()
-print(len(startt))
-if(len(startt)>0){
-  endtt=EnterStartStop("Enter Time Duration (secs) or (mm:ss)\n")
-  unlink('~/temppt.mp4')
-  file.rename(svt,'~/temppt.mp4')
-  svtt1=gsub(' ','',svt) # remove spaces for ffmpeg (does not accept " in filename's)
-  svtt='c:/RealPlayerDownloads/trimmed.mp4'
-  entf=FALSE
-  if(len(endtt)==0){
-    entf=TRUE
-    endtt=10000
-  }
-  cmdd=paste('shell("ffmpeg.exe -ss',startt,' -i c:/users/LarrySloman/Documents/temppt.mp4 -t',endtt,'-c:v copy -c:a copy',svtt,'",mustWork=NA,translate=TRUE)')
-  print(cmdd)
-  eval(parse(text=cmdd))
-  if(entf){
-    file.rename(svtt,svt) # replace svt has trimmed with start to end
-  }else{
-    file.rename('~/temppt.mp4',svt)  # put back original, trimmed.mp4 has trimmed
-  }
-}
-
+if(is.na(svt))
+  svt=file.choose()
+print(paste('svt=',svt))
+StartMyGUI()
+gtkMain()
