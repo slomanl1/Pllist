d4f23d6693a67067d0bb1c4ca29ffa4121ebfc8a
slomanl1 <slomanl75@gmail.com>
2015-11-17 20:36:44 -0800

    first working version Major revs added tools library for --vanilla rscript.exe keep original file

--- trimmer.R
+++ trimmer.R
@@ -1,7 +1,8 @@
+len=function(x) length(x)
 EnterStartStop = function(x="Enter Start Time (secs) or (mm:ss)\n"){
   .GlobalEnv$ss=NULL
   while(TRUE){
-    ginput(x, icon="question", handler = function(h,...) .GlobalEnv$ss=h$input)
+    ginput(x, icon="question", title=svt, handler = function(h,...) .GlobalEnv$ss=h$input)
     startt= .GlobalEnv$ss   
     if(len(startt)>0){
       if(!is.na(as.integer(startt))){
@@ -29,31 +30,28 @@ StartMyGUI <- function(handler=function(h,...) {
   startt=EnterStartStop()
   print(len(startt))
   if(len(startt)>0){
-    endtt=EnterStartStop("Enter Time Duration (secs) or (mm:ss)\n")
-    unlink('~/temppt.mp4')
-    file.rename(svt,'~/temppt.mp4')
-    svtt1=gsub(' ','',svt) # remove spaces for ffmpeg (does not accept " in filename's)
+    endtt=EnterStartStop("Enter Time Duration (secs) or (mm:ss), or Esc for End of File\n")
     svtt='c:/RealPlayerDownloads/trimmed.mp4'
-    entf=FALSE
+    unlink('~/temppt.mp4')
+    unlink(svtt)
+    file.rename(svt,'~/temppt.mp4')
     if(len(endtt)==0){
-      entf=TRUE
       endtt=10000
     }
     cmdd=paste('shell("ffmpeg.exe -ss',startt,' -i c:/users/LarrySloman/Documents/temppt.mp4 -t',endtt,'-c:v copy -c:a copy',svtt,'",mustWork=NA,translate=TRUE)')
     print(cmdd)
     eval(parse(text=cmdd))
-    if(entf){
-      file.rename(svtt,svt) # replace svt has trimmed with start to end
-    }else{
-      file.rename('~/temppt.mp4',svt)  # put back original, trimmed.mp4 has trimmed
+    svtO=paste(file_path_sans_ext(svt),'_New.',file_ext(svt),sep='') # add _New to original filename
+    file.rename(svtt,svtO) # replace svt has trimmed with start to end
+    file.rename('~/temppt.mp4',svt) # keep original file
     }
-  }
   return()
 } 
 options("guiToolkit"="RGtk2")
 library(RGtk2)
 library(gWidgets)
 library(gWidgetsRGtk2)
+library(tools)
 args <- commandArgs(TRUE)
 save(args,file='~/args.RData')
 svt=args[1]
@@ -61,4 +59,4 @@ if(is.na(svt))
   svt=file.choose()
 print(paste('svt=',svt))
 StartMyGUI()
-gtkMain()
+#gtkMain()
