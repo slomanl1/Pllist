eed9467f73660ebdfb9e3c6f1f9b1e3d1fefb334
slomanl1 <slomanl75@gmail.com>
2015-11-29 16:32:09 -0800

    Moved orderallwpl to separate routine reuseable

--- makeWPLS.R
+++ makeWPLS.R
@@ -69,24 +69,6 @@ save(mfnfo,wpls,file='~/fnfo.RData')
 setwd(paste(pldrive,'My Playlists',sep=""))
 
 ###################### orderall ######################
-for (j in 1:length(wpls)) {
-  print(wpls[j])
-  fnn=readLines(wpls[j])
-  strt=grep('media',fnn)[1]
-  lss=fnn[strt:(length(fnn)-3)]
-  lssx=''
-  for(i in 1:length(lss)){
-    lssx[i]=substr(lss[i],regexpr('Clips',lss[i])[1]+6,regexpr('mpg|mp4|flv|asf|wmv',lss[i])[1]+2)
-  }
-  setwd(paste(drive,'My Videos/RPDNClips',sep=""))
-  lssy=sub('_New','',lssx)
-  dxx=data.frame(lsst=lssy,lss)
-  lssj=merge(dxx,mfnfo[,c('lsst','mtime')])
-  lssz=as.character(lssj[order(lssj$mtime),'lss'])
-  fnnh=fnn[1:(strt-1)] #wpl header
-  fnnt=fnn[(length(fnn)-2):length(fnn)] #wpl footer
-  fnno=c(fnnh,sub('..\\My','C:\\My',lssz,fixed = TRUE),fnnt)
-  setwd(paste(pldrive,'My Playlists',sep=""))
-  writeLines(fnno,wpls[j])
-}
+source('~/pllist.git/orderallwpl.R')
+
 
