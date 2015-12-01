6c1bdcd630c9499d9736660628139f39023a7a4a
slomanl1 <slomanl75@gmail.com>
2015-11-30 16:06:02 -0800

    minor - remove zz.txt after use to save disk space

--- MakeDfanFromAM.R
+++ MakeDfanFromAM.R
@@ -66,6 +66,7 @@ if (file.exists('D:/PNMTALL')) {
     shell(paste('dir', 'D:\\PNMTALL',' /S/B/OD >  zz.txt'))
     shell(paste('dir', 'C:\\PNMTALL',' /S/B/OD >> zz.txt'))
     zz1 = readLines('zz.txt')
+    unlink('zz.txt')
     zz2 = zz1[which(grepl('.',zz1,fixed = TRUE) &
                       !grepl('RECYCLE|.txt|.RData|RPDN|.tmp',zz1,fixed=TRUE) &
                       toupper(dirname(zz1)) %in% toupper(normalizePath((dirs),winslash = '/')))]
