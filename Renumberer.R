5b22fa8227f43ff20fed1c2b2e6b0d87beba04e3
slomanl1 <slomanl75@gmail.com>
2015-12-16 22:09:48 -0800

    new mfnfo structure

--- Renumberer.R
+++ Renumberer.R
@@ -3,41 +3,42 @@ source(scriptStatsRemoveAll) #clear bones
 require(bitops)
 source("~/Local.R")
 setwd('c:/my videos/rpdnclips')
-load('~/fnfo.RData') # load fnfo, lsst, wpls and xx
-lsst=fnfo$lsst
+load('~/mfnfo.RData') # load fnfo, lsst, wpls and xx
+lsst=mfnfo$lsst
 bn=gsub('[a-z|A-Z]','',lsst)
 bc=gsub('[0-9]','',lsst)
-bn=substr(bn,1,nchar(bn)-1)
-namer=fnfo
-namer$bn=bn
+bn=substr(bn,1,nchar(bn)-2)
+namer=mfnfo
+namer$bn=as.integer(bn)
 namer$info=bc
 namer=namer[order(namer$mtime),]
-namer$nfn=1:nrow(namer)
-if(grepl('_New',lsst[1])){
-  namer$nfn=gsub('_New.','.',paste(namer$nfn,namer$info,sep=''),fixed=TRUE)
-}else{
-  namer$nfn=gsub('.','_New.',paste(namer$nfn,namer$info,sep=''),fixed=TRUE)
-}
-lsst=namer$nfn
+#namer=namer[order(namer$bn),]
+namer$newfn=1:nrow(namer)
+namer$newfn=paste(namer$newfn,'_REN',namer$info,sep='')
+namer$ofn=NA
+namer[file.exists(namer$lsst),]$ofn=namer[file.exists(namer$lsst),]$lsst
+namer[file.exists(namer$newfn),]$ofn=namer[file.exists(namer$newfn),]$newfn
+
 answ=gconfirm('RENAME .wpl - Are you Sure?')
 if(answ){
-  rnmd=file.rename(as.character(namer$lsst),as.character(namer$nfn))
+  rnmd=file.rename(as.character(namer$ofn),as.character(namer$newfn))
   if(any(!rnmd))
   {
     print('File Rename Failed')
   }else{
-    fnfon=file.info(lsst)
-    fnfon$lsst=lsst
-    fnfog=merge(namer,fnfo,by='lsst')
-    fnfog$lsst=fnfog$nfn
-    nms=subset(names(fnfog),!grepl('.y',names(fnfog)),fixed=TRUE)
-    fnfoh=fnfog[,nms]
-    names(fnfoh)=gsub('.x','',names(fnfoh),fixed=TRUE)
-    fnfo=fnfoh[,names(fnfo)]
+    lsst=namer$newfn
+    mfnfon=file.info(lsst)
+    mfnfon$lsst=lsst
+    mfnfog=merge(namer,mfnfo,by='lsst')
+    mfnfog$lsst=mfnfog$newfn
+    nms=subset(names(mfnfog),!grepl('.y',names(mfnfog)),fixed=TRUE)
+    mfnfoh=mfnfog[,nms]
+    names(mfnfoh)=gsub('.x','',names(mfnfoh),fixed=TRUE)
+    mfnfo=mfnfoh[,names(mfnfo)]
     setwd(paste(pldrive,'My Playlists',sep=""))
     wpls = sort(dir(pattern = '*.wpl'))
-    save(fnfo,wpls,file='~/fnfo.RData')
-    save(namer,fnfo,wpls,file=paste('~/namer',gsub(':','_',Sys.time(),fixed=TRUE),'.RData',sep=''))
+    save(mfnfo,wpls,file='~/mfnfo.RData')
+    save(namer,mfnfo,wpls,file=paste('~/namer',gsub(':','_',Sys.time(),fixed=TRUE),'.RData',sep=''))
     source('~/pllist.git/makeWPLS.R')
   }
 }else{
