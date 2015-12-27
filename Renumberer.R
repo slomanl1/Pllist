510cd13e0644f85c05a67a8720d5add1122a62d2
slomanl1 <slomanl75@gmail.com>
2015-12-26 21:48:01 -0800

    Fixed namer time order ( dup times apart by one second

--- Renumberer.R
+++ Renumberer.R
@@ -10,22 +10,22 @@ bn=gsub('[a-z|A-Z]','',lsstn)
 bc=gsub('[0-9]','',lsstn)
 bn=substr(bn,1,nchar(bn)-2)
 namer=mfnfo
+namer$mtime=file.mtime(mfnfo$lsst)
+namer$lsst=gsub('_REN','',namer$lsst)
 namer$bn=as.integer(bn)
 namer$info=bc
-namer=namer[order(namer$mtime),]
+
 while(any(duplicated(namer$mtime))){ # ensure that no two entires are equal, sep by 1 second
   namer$mtime=namer$mtime+duplicated(namer$mtime)
 }
-
+namer=namer[order(as.character(namer$mtime)),]
 namer$newfn=1:nrow(namer)
 namer$newfn=paste(namer$newfn,'_REN',namer$info,sep='')
 namer$ofn=NA
 namer[file.exists(namer$lsst),]$ofn=namer[file.exists(namer$lsst),]$lsst
 namer[file.exists(namer$newfn),]$ofn=namer[file.exists(namer$newfn),]$newfn
 rens=which(grepl('_REN',namer$ofn))
-norens=which(!grepl('_REN',namer$ofn))
-# if(len(norens))
-#   namer[norens,]$newfn=paste(namer[norens,]$newfn,'_REN',namer[norens,]$info,sep='')
+
 if(len(rens))
   namer[rens,]$newfn=gsub('_REN','',namer[rens,]$newfn)
 answ=gconfirm('RENAME .wpl - Are you Sure?')
@@ -35,10 +35,7 @@ if(answ){
   {
     print('File Rename Failed')
   }else{
-    lsst=namer$newfn
-    mfnfon=file.info(lsst)
-    mfnfon$lsst=lsst
-    mfnfog=merge(namer,mfnfo,by='lsst')
+    mfnfog=merge(namer,mfnfo,by.x='ofn',by.y='lsst')
     mfnfog$lsst=mfnfog$newfn
     nms=subset(names(mfnfog),!grepl('.y',names(mfnfog)),fixed=TRUE)
     mfnfoh=mfnfog[,nms]
