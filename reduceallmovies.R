a025eba7692c11af4ab312dd0fd0500eb66a605f
slomanl1 <slomanl75@gmail.com>
2016-01-07 21:37:24 -0800

    Test version, new clip processing, file date fix (fdd.bat)

--- reduceallmovies.R
+++ reduceallmovies.R
@@ -1,8 +1,8 @@
 cd('~/')
 cls=NULL
 dwnlds=NULL
-cls=c(cls,dir('D:/PNMTALL',recursive = TRUE,full.names = TRUE))
-cls=c(cls,dir('c:/PNMTALL',recursive = TRUE,full.names = TRUE))
+#cls=c(cls,dir('D:/PNMTALL',recursive = TRUE,full.names = TRUE))
+#cls=c(cls,dir('c:/PNMTALL',recursive = TRUE,full.names = TRUE))
 cls=c(cls,dir('c:/my videos/rpdnclips',recursive = TRUE,full.names = TRUE))
 dwnlds1=dir('c:/Users/Larry/Downloads',recursive = TRUE,full.names = TRUE)
 dwnlds=dwnlds1[grepl('REDUCE',dwnlds1)]
@@ -21,6 +21,10 @@ nfns=paste(file_path_sans_ext(cls),'_New.',file_ext(cls),sep='')
 dfa=data.frame(cls,sz=file.size(cls),nfns)
 dfa=subset(dfa,!file.exists(as.character(dfa$nfns))) # remove already converted to _New
 dfa=dfa[order(dfa$sz,decreasing=FALSE),]
+nfns=paste(file_path_sans_ext(cls),'_New.',file_ext(cls),sep='')
+dfa1=data.frame(cls,sz=file.size(cls),nfns)
+dfa1=subset(dfa1,!file.exists(as.character(dfa1$nfns))) # remove already converted to _New
+dfa=rbind(dfa1,dfa)
 
 bads=data.frame(fname=NA,errorC=NA)
 if(file.exists('~/bads.RData'))
@@ -30,18 +34,28 @@ dfa=dfa[!toupper(dfa$cls) %in% toupper(bads$fname),]
 if(!file.exists('~/msgis.txt')){
   writeLines('','~/msgis.txt')
 }
+dfa=dfa[!duplicated(dfa$cls),]
+dfa=dfa[order(dfa$sz,decreasing=TRUE),]
 for(fn in dfa$cls)
 { 
   print('')
   print(paste(len(dfa$cls)-which(fn==dfa$cls),'Files Remaining',
               ptn(sum(file.size(as.character(dfa$cls)),na.rm = TRUE)/1000),'Kbytes Remaining',Sys.time()))
+  
   nfn1=paste(file_path_sans_ext(fn),'_New.',file_ext(fn),sep='')
   nfn=sub('REDUCE','',nfn1)
+  clflag=FALSE
+  if(grepl('rpdnclips',nfn)){
+    clflag=TRUE
+    nfn=sub('_New','',nfn)
+  }
   print(paste(fn,ptn(file.size(fn)),'nfn-',nfn))
   ddd=shell(paste('mediainfo "',fn,'"',sep=''),intern = TRUE)
   hevcFlag=any(grepl('HEVC',ddd))
-  if(file.exists(fn) & !file.exists(nfn) & file.size(fn)==dfa[which(dfa$cls==fn),'sz']){
+  if((clflag & !hevcFlag) | file.exists(fn) & !file.exists(nfn) & file.size(fn)==dfa[which(dfa$cls==fn),'sz']){
     if(!hevcFlag){
+      mtime=file.mtime(fn)
+      print(paste('Mtime=',mtime))
       print(subset(ddd,grepl('Duration',ddd))[1])
       of=paste(basename(tempfile()),'.mp4',sep='')
       print(of)
@@ -51,11 +65,22 @@ for(fn in dfa$cls)
       if(file.exists(of)){
         if(file.size(of)>1000){
           if(any(grepl('hevc',msgi))){
-            if(file.copy(of,nfn))
+            unlink(nfn)
+            if(file.copy(of,nfn)){
               unlink(of)
-            print(paste(fn,ptn(file.size(fn))))
-            print(paste(nfn,ptn(file.size(nfn))))
-            file.remove(fn)
+              dx=data.frame(dtn=NA,fn=NA,times=NA)
+              dx$dtn=mtime
+              dx$fn=normalizePath(as.character(fn),winslash = '/')
+              dx$times=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
+                             ' I:',as.POSIXlt(dx$dtn)$min,' S:' ,as.POSIXlt(dx$dtn)$sec,sep='')
+              shell(paste('c:/Users/Larry/Documents/fdd.bat "',
+                          nfn,'" ',dx$times,',' ,sep=''),translate = TRUE, intern = TRUE)
+
+              print(paste('file mtime back to orig',file.mtime(nfn)))
+              print(paste(fn,ptn(file.size(fn))))
+              print(paste(nfn,ptn(file.size(nfn))))
+              file.remove(fn)
+            }
           }else{
             bads[badx,]$fname=fn
             bads[badx,]$errorC='Bad Metadata'
