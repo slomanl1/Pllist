052871ee63a58d5c1af8ad1c6e172879163b0f04
slomanl1 <slomanl75@gmail.com>
2016-01-18 23:10:00 -0800

    First working check-in with Fdate processing Added require gdata for standalone operation

---
+++ dfxprocess.R
@@ -0,0 +1,24 @@
+source('~/pllist.git/EnterStartStop.R')
+require(gdata)
+
+dx=data.frame(dtn=NA,fn=NA,times=NA)
+if(!exists('Fdate')){
+  Fdate=FALSE
+}else{
+  filename=svt
+}
+if(!Fdate)
+  filename=file.choose()
+Fdate=FALSE
+ALTGinput("Enter mod date/time", TRUE) 
+print(paste('ss=',ss,filename))
+
+dx[1,'dtn']=ss
+dx$dtn=as.POSIXlt(dx$dtn)+(8*3600) # add 8 hours to make GMT
+dx[1,'fn']=normalizePath(as.character(filename),winslash = '/')
+dx[1,'times']=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
+               ' I:',as.POSIXlt(dx$dtn)$min,' S:' ,as.POSIXlt(dx$dtn)$sec,sep='')
+
+cmd=paste('shell(','"fdate',dx$fn,dx$times,'")')
+eval(parse(text=cmd))
+
