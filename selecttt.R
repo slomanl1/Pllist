f729de3d50bc5ddd31dc4606359e82036821f3fa
slomanl1 <slomanl75@gmail.com>
2016-01-26 22:44:15 -0800

    Added rasio buttions on initial sfanme rebuild choice

--- selecttt.R
+++ selecttt.R
@@ -1,4 +1,4 @@
-flavors <- c("NO","YES")
+flavors <- c("Choose","NO","YES")
 avail=FALSE
 vall='NO'
 flagg=TRUE
@@ -11,21 +11,21 @@ f <- function(h,...) {
 xw <- gwindow("Delete sfname and rebuild?",height = 20)
 gp <- ggroup(container=xw)
 
-cb <- gcombobox(flavors, editable=TRUE, container=gp, handler=f)
+cb <- gradio(flavors, editable=TRUE, container=gp, handler=f)
 enabled(cb)=FALSE
 addHandlerDestroy(xw, handler = function(h,...) {
   .GlobalEnv$destroyed=.GlobalEnv$flagg
   .GlobalEnv$avail=TRUE
 })
 cntt=7
-delay500()
+Sys.sleep(1)
 shell('nircmd win activate title "Delete sfname and rebuild?"')
 enabled(cb)=TRUE
 focus(cb)=TRUE
 while(!avail)
 {
   svalue(xw)=paste("Delete sfname and rebuild?",cntt)
-  delay500()
+  Sys.sleep(1)
   cntt=cntt-1
   if(cntt==0){
     flagg=FALSE
