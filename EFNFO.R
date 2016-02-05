291ee3d03b94cbc34791b417ec3133b3fdd82e96
slomanl1 <slomanl75@gmail.com>
2016-02-04 22:13:43 -0800

    removed

--- EFNFO.R
+++ EFNFO.R
@@ -17,46 +17,53 @@ mgdn1=subset(mgdf,grepl('_New',filename.y))
 # emg[!is.na(emg$tdif) & emg$tdif>0,'mtime']=emg[!is.na(emg$tdif) & emg$tdif>0,'Dmtime']
 # efnfo=emg[,names(efnfo)]
 # save(efnfo,file='~/efnfo.RData')
-if(nrow(mssg)>0){
+mgdnx=mgdn1[0,]
+while(nrow(mssg)>0){
+  mgdn2=merge(mgdn1,mssg,by='bn',all.x = TRUE)
+  mgdn2$mtime.x=as.character(mgdn2$mtime.y)
+  # get e: filenames and mtime from filename.x.y and mtime.y
+  mgdn2[is.na(mgdn2$filename.x.x),'filename.x.x']=mgdn2[is.na(mgdn2$filename.x.x),'filename.x.y']
+  mgdn2[is.na(mgdn2$filename.x.x),'mtime.x']=mgdn2[is.na(mgdn2$filename.x.x),'mtime.y']
+  mgdn=mgdn2[,c('bn','filename.x.x',"size","isdir","mode","mtime.x","ctime","atime",
+                "exe","filename.y.x","dtitle.x","Dmtime.x" )]
+  #names(mgdn)=names(mgdn1) ########
+  mgdn3=mgdn[is.na(mgdn$filename.x.x),]
+  agg=mgdn3[which(sapply(1:(nrow(mgdn3)),
+                         function(x) return(agrepl(mgdn3$dtitle[x],mgdn3$bn[x],ignore.case = TRUE,
+                                                   max.distance = .6)))==1),c('filename.y.x','dtitle.x','bn')]
+  #-----------------------------------------------------------------------------------------
+  print(agg)
+  agg=agg[!agg$bn %in% mssg$bn,]
+  j=nrow(agg)
+  i=nrow(mssg)+1
+  k=1
+  while(j>0){
+    mssg[i,]=NA
+    mssg[i,]$bn=agg[k,]$bn
+    mssg[i,]$filename.y=normalizePath(as.character(agg[k,]$filename.y.x),winslash = '/')
+    mssg[i,]$dtitle=agg[k,]$dtitle.x
+    mssg[i,]$Dmtime=file.mtime(as.character(mssg[i,]$filename.y))
+    i=i+1
+    j=j-1
+    k=k+1
+  }
   fix(mssg)
   save(mssg,file='~/mssg.RData')
-}
-mgdn2=merge(mgdn1,mssg,by='bn',all.x = TRUE)
-mgdn2$mtime.x=as.character(mgdn2$mtime.y)
-# get e: filenames and mtime from filename.x.y and mtime.y
-mgdn2[is.na(mgdn2$filename.x.x),'filename.x.x']=mgdn2[is.na(mgdn2$filename.x.x),'filename.x.y']
-mgdn2[is.na(mgdn2$filename.x.x),'mtime.x']=mgdn2[is.na(mgdn2$filename.x.x),'mtime.y']
-mgdn=mgdn2[,c('bn','filename.x.x',"size","isdir","mode","mtime.x","ctime","atime",
-              "exe","filename.y.x","dtitle.x","Dmtime.x" )]
-#names(mgdn)=names(mgdn1) ########
-mgdn3=mgdn[is.na(mgdn$filename.x),]
-agg=mgdn3[which(sapply(1:(nrow(mgdn3)),
-                       function(x) return(agrepl(mgdn3$dtitle[x],mgdn3$bn[x],ignore.case = TRUE,
-                                                 max.distance = .6)))==1),c('filename.y.x','dtitle.x','bn')]
-#-----------------------------------------------------------------------------------------
-print(agg)
-agg=agg[!agg$bn %in% mssg$bn,]
-j=nrow(agg)
-i=nrow(mssg)+1
-k=1
-while(j>0){
-  mssg[i,]=NA
-  mssg[i,]$bn=agg[k,]$bn
-  mssg[i,]$filename.y=normalizePath(as.character(agg[k,]$filename.y.x),winslash = '/')
-  mssg[i,]$dtitle=agg[k,]$dtitle.x
-  mssg[i,]$Dmtime=file.mtime(as.character(mssg[i,]$filename.y))
-  i=i+1
-  j=j-1
-  k=k+1
+  mgdnx=mgdn[!is.na(mgdn$mtime.x),]
+  if(nrow(mgdnx)==0)
+    next
+  dd=paste(sub(mgdnx$bn,'',mgdnx$filename.y.x,fixed=TRUE),mgdnx$bn,sep='')
+  mgdn1[mgdn1$bn %in% mgdnx$bn,]$filename.x=sub(substr(dd,1,1),'E',dd)
+  mgdn1[mgdn1$bn %in% mgdnx$bn,]$mtime=mgdnx$mtime.x
+  mssg=mssg[!mssg$bn %in% mgdnx$bn,] # remove entries with filename filled in manually
   save(mssg,file='~/mssg.RData')
 }
-
-# consider adding agg to mssg to allow mtile entry manual or from matching E: dates
-
-mgdnx=mgdn[!is.na(mgdn$mtime.x),]
 ### GET title from EDrive backup of originals of e:/PNMTALL of bn in print expression above
-reverter=mgdnx[abs(as.POSIXlt(mgdnx$mtime.x)-as.POSIXlt(mgdnx$Dmtime.x))>40,c('filename.y.x','mtime.x','Dmtime.x')]
+dx=mgdnx
+if(nrow(mgdnx)>0){
+  reverter=mgdnx[abs(as.POSIXlt(mgdnx$mtime.x)-as.POSIXlt(mgdnx$Dmtime.x))>40,c('filename.y.x','mtime.x','Dmtime.x')]
 dx=subset(reverter,!is.na(mtime.x))
+}
 if(nrow(dx)>0){
   dx$dtn=as.POSIXlt(dx$mtime.x)
   dx$fn=normalizePath(as.character(dx$filename.y.x),winslash = '/')
