0c72a2f814984062bfb35bb7a7b36e26a664c834
slomanl1 <slomanl75@gmail.com>
2016-02-04 16:36:48 -0800

    Change radio buttons to yes/no red/green gbuttons

--- selecttt.R
+++ selecttt.R
@@ -11,8 +11,12 @@ f <- function(h,...) {
 xw <- gwindow("Delete sfname and rebuild?",height = 20)
 gp <- ggroup(container=xw)
 
-cb <- gradio(flavors, editable=TRUE, container=gp, handler=f)
-enabled(cb)=FALSE
+#cb <- gradio(flavors, editable=TRUE, container=gp, handler=f)
+yb=gbutton("YES", container=gp, handler = f)
+font(yb) <- c(color="red" , style="bold")
+cb=gbutton("NO", container=gp, handler = f)
+font(cb) <- c(color="springgreen4" , style="bold")
+
 addHandlerDestroy(xw, handler = function(h,...) {
   .GlobalEnv$destroyed=.GlobalEnv$flagg
   .GlobalEnv$avail=TRUE
