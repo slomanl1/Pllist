2a3ce9478811630d0660fa3b8cfd415262e68ec9
slomanl1 <slomanl75@gmail.com>
2016-03-15 20:22:44 -0700

    Added pre-select logic and toggle OK button

--- ChooseDIRS.R
+++ ChooseDIRS.R
@@ -1,22 +1,27 @@
 destr=FALSE
 sll=NA
+
 ww=gwindow('Choose',height=180,width=700)
-#cb=rep(TRUE,len(choices))
-#for(i in 1:len(choices))
-#  gcheckbox(choices[i],container=ww,action=i,checked=TRUE,handler=function(h,...){.GlobalEnv$cb[svalue(h$action)]=svalue(h$obj)})
 txl=gtable(choices,container=ww,multiple = TRUE,handler=function(h,...){.GlobalEnv$cb=svalue(h$obj)})
 vv=gradio(c('INCREASING','DECREASING'),container = ww)
-gcheckbox('OK',container=ww,handler=function(h,...){gtkMainQuit()})
+gcheckbox('OK',container=ww,use.togglebutton=TRUE,handler=function(h,...){gtkMainQuit()})
 addhandlerdestroy(ww,handler=function(h,...){
   .GlobalEnv$destr=TRUE
   gtkMainQuit()
 })
+if(file.exists('~/ChooseDIRS.RData')){
+  load('~/ChooseDIRS.RData') # load pre-select from sll
+  svalue(txl)=sll
+  svalue(vv)=slv
+}
 gtkMain()
 if(!destr){
   decreasing=FALSE
   if(svalue(vv)=='DECREASING')
     decreasing=TRUE
   sll=svalue(txl)
+  slv=svalue(vv)
+  save(sll,slv,file='~/ChooseDIRS.RData')
   if(exists('ww'))
     dispose(ww)
 }
