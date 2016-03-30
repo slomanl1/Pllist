26090f1b0379fd6d99e81b93c3b75327e49c66af
slomanl1 <slomanl75@gmail.com>
2016-03-30 09:20:56 -0700

    Add Y/N confirm

--- DetachAllPackages.R
+++ DetachAllPackages.R
@@ -6,8 +6,16 @@ detachAllPackages <- function() {
   
   package.list <- setdiff(package.list,basic.packages)
   
+  print(package.list)
+  print('are you sure? Y/N')
+  cnf=readline()
+  if(cnf=='y' | cnf=='Y'){
   if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
-  
+  }else
+  {
+    print('No action taken')
+  }
+
 }
 
 detachAllPackages()
