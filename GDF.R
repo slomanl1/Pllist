2f3f23a324d56852899408189bcded004bf30323
slomanl1 <slomanl75@gmail.com>
2016-03-30 23:12:24 -0700

    New gdfd using gdf() needs done button

--- GDF.R
+++ GDF.R
@@ -1,50 +1,20 @@
 gdfd=function(dfx) {
   .GlobalEnv$dfy=dfx
-  linerw=gwindow(height = 30, title='Edit Record')
-  ggp=ggroup(cont=linerw)
-  obj1 =  gedit(dfx[,1], width=nchar(trim(dfx[,1]))*1.15,editable=TRUE, container = ggp)
-  addHandlerChanged(obj1,handler=function(h,...){
-    .GlobalEnv$dfy[,1]=svalue(obj1)
-    gtkMainQuit()})
-  obj2 =  gedit(dfx[,2], editable=TRUE, container = ggp)
-  addHandlerChanged(obj2,handler=function(h,...){
-    .GlobalEnv$dfy[,2]=svalue(obj2)
-    gtkMainQuit()})
-  obj3=  gedit(dfx[,3], editable=TRUE, container = ggp)
-  addHandlerChanged(obj3,handler=function(h,...){
-    .GlobalEnv$dfy[,3]=svalue(obj3)
-    gtkMainQuit()})
-  obj4=  gedit(dfx[,4], editable=TRUE, container = ggp)
-  addHandlerChanged(obj4,handler=function(h,...){
-    .GlobalEnv$dfy[,4]=svalue(obj4)
-    gtkMainQuit()})
-
-  
-  
-  addHandlerBlur(obj1,handler=function(h,...){
-    .GlobalEnv$dfy[,1]=svalue(obj1)
+  for(x in 1:ncol(dfx)){
+    if(is.na(dfx[,x]))
+      dfx[,x]=' '
+  }
+  hx=gwindow(height=30,width=1080)
+  hy=gdf(dfx[,1:4],cont=hx)
+  addHandlerChanged(hy, handler = function(h,...) {
+    print(svalue(h$obj, drop = FALSE))
+    .GlobalEnv$dfy=svalue(h$obj,drop = FALSE)
   })
+  IDD=addHandlerDestroy(hx,handler=function(h,...) {
+    print('hx destroyed');
+    gtkMainQuit()})
   
-  addHandlerBlur(obj2,handler=function(h,...){
-    .GlobalEnv$dfy[,2]=svalue(obj2)
-  })
-  
-  addHandlerBlur(obj3,handler=function(h,...){
-    .GlobalEnv$dfy[,3]=svalue(obj3)
-  })
-  
-  addHandlerBlur(obj4,handler=function(h,...){
-    .GlobalEnv$dfy[,4]=svalue(obj4)
-  })
-  
-
-  IDD=addHandlerDestroy(linerw,handler=function(h,...) {gtkMainQuit()})
   gtkMain()
-  
-  if(isExtant(linerw))
-      dispose(linerw)
   dfyy=.GlobalEnv$dfy
-  rm('.GlobalEnv$dfy')
   return(dfyy)
 }
-
