519cc2f427acb6a7016d117cbda2a454be698e8d
slomanl1 <slomanl75@gmail.com>
2016-05-29 09:56:38 -0700

    Moved tailfail to gui trimmer

--- StartMyGuiTrimmer.R
+++ StartMyGuiTrimmer.R
@@ -134,3 +134,34 @@ StartMyGUI <- function() {
     return()
   }
 }
+
+tailfile=function(file, n=6) {
+  bufferSize <- 1024L
+  size <- file.info(file)$size
+  
+  if (size < bufferSize) {
+    bufferSize <- size
+  }
+  
+  pos <- size - bufferSize
+  text <- character()
+  k <- 0L
+  
+  f <- file(file, "rb")
+  on.exit(close(f))
+  
+  while(TRUE) {
+    seek(f, where=pos)
+    chars <- readChar(f, nchars=bufferSize)
+    k <- k + length(gregexpr(pattern="\\n", text=chars)[[1L]])
+    text <- paste0(text, chars)
+    
+    if (k > n || pos == 0L) {
+      break
+    }
+    
+    pos <- max(pos-bufferSize, 0L)
+  }
+  
+  tail(strsplit(text, "\\n")[[1L]], n)
+}
