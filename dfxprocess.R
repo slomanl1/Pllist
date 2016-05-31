70073399395cf77a395551983d19286d65c66478
slomanl1 <slomanl75@gmail.com>
2016-05-30 21:58:23 -0700

    GMT

--- dfxprocess.R
+++ dfxprocess.R
@@ -14,7 +14,7 @@ ALTGinput("Enter mod date/time", TRUE)
 print(paste('ss=',ss,filename))
 
 dx[1,'dtn']=ss
-dx$dtn=as.POSIXlt(dx$dtn)+(8*3600) # add 8 hours to make GMT
+dx$dtn=as.POSIXlt(dx$dtn)+(7*3600) # add 7 hours to make GMT
 dx[1,'fn']=normalizePath(as.character(filename),winslash = '/')
 dx[1,'times']=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
                ' I:',as.POSIXlt(dx$dtn)$min,' S:' ,as.POSIXlt(dx$dtn)$sec,sep='')
