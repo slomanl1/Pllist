8637a77cc1d84b6e783513c016ae67b7f116846e
slomanl1 <slomanl75@gmail.com>
2016-07-15 18:36:16 -0700

    Error trap for invalid dates

--- dfxprocess.R
+++ dfxprocess.R
@@ -10,15 +10,16 @@ if(!exists('Fdate')){
 if(!Fdate)
   filename=file.choose()
 
-ALTGinput("Enter mod date/time", TRUE) 
-print(paste('ss=',ss,filename))
-
-dx[1,'dtn']=ss
-dx$dtn=as.POSIXlt(dx$dtn)+(7*3600) # add 7 hours to make GMT
-dx[1,'fn']=normalizePath(as.character(filename),winslash = '/')
-dx[1,'times']=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
-               ' I:',as.POSIXlt(dx$dtn)$min,' S:' ,as.POSIXlt(dx$dtn)$sec,sep='')
-
-cmd=paste('shell(','"fdate',dx$fn,dx$times,'")')
-eval(parse(text=cmd))
-
+ALTGinput("Enter mod date/time", TRUE)
+if(len(ss)){
+  print(paste('ss=',ss,filename))
+  
+  dx[1,'dtn']=ss
+  dx$dtn=as.POSIXlt(dx$dtn)+(7*3600) # add 7 hours to make GMT
+  dx[1,'fn']=normalizePath(as.character(filename),winslash = '/')
+  dx[1,'times']=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
+                      ' I:',as.POSIXlt(dx$dtn)$min,' S:' ,as.POSIXlt(dx$dtn)$sec,sep='')
+  
+  cmd=paste('shell(','"fdate',dx$fn,dx$times,'")')
+  eval(parse(text=cmd))
+}
