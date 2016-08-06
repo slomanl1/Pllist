e8d6e2ba7c5518990dc71d29f2ae01910a5cd64d
slomanl1 <slomanl75@gmail.com>
2016-08-05 23:45:52 -0600

    Beta test

--- Concat.R
+++ Concat.R
@@ -1,5 +1,27 @@
-fls=choose.files(caption='Choose files to concatenate')
-if(len(fls)>1){
+idd="C:/PNMTALL/RPDNCLIPS"
+flss=NULL
+fls=NULL
+while(TRUE){
+  idd=choose.dir(idd,caption='Choose Input Directory')
+  if(is.na(idd))
+    break
+  print(paste('idd chosen=',idd))
+  cd(idd)
+  cd()
+  while(!is.na(idd)){
+    print(paste('idd chosen=',idd))
+    fls=choose.files()
+    
+    if(!len(fls))
+      break
+    flss=c(flss,fls)
+    tbl=gtable(flss,cont=gwindow())
+  }
+  if(!len(fls))
+    break
+}
+
+if(len(flss)>1){
   od=choose.dir('~/',caption='Choose Output Directory')
   if(!is.na(od)){
     svtti=ginput('Enter output file name (.mp4)')
@@ -8,9 +30,9 @@ if(len(fls)>1){
     writeLines(' ',ofx)
     of=normalizePath(ofx,winslash='/')
     unlink(ofx)
-    if(len(svtt)){
+    if(len(svtt) & len(svtti)){
       fls=paste("file '",fls,"'",sep='')
-      writeLines(fls,'~/mylist.txt') 
+      writeLines(flss,'~/mylist.txt') 
       cmdd=paste('shell("ffmpeg.exe -f concat -i c:/users/Larry/Documents/mylist.txt -c copy',of,'",mustWork=NA,translate=TRUE)')
       print(cmdd)
       eval(parse(text=cmdd))
@@ -20,6 +42,8 @@ if(len(fls)>1){
   }else{
     print('no directory chosen')
   }
+  dispose(tbl)
 }else{
   print('Invalid Input, at least 2 files required')
 }
+
