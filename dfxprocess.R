ef77ffe337df62c186e17c40950115b6c2558a97
slomanl1 <slomanl75@gmail.com>
2020-05-30 21:08:38 -0600

    Fixed DST

--- dfxprocess.R
+++ dfxprocess.R
@@ -21,7 +21,7 @@ if(len(ss)){
   dx=data.frame(dtn=NA,fn=NA,times=NA)
   dx[1,'dtn']=ss
   dx$dtn=as.POSIXlt(dx$dtn)+(6*3600) # add 6 hours to make GMT
-  if(!isDST())
+  if(!dst(dx$dtn))
     dx$dtn=dx$dtn+3600 # add one more hour for Standard Time (MST)
   dx[1,'fn']=normalizePath(as.character(filename),winslash = '/')
   dx[1,'times']=paste('Y:',getYear(dx$dtn),' M:',getMonth(dx$dtn),' D:',getDay(dx$dtn),' H:',as.POSIXlt(dx$dtn)$hour,
