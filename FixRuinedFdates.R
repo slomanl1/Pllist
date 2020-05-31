27b774e5acfa5254b3eae84a88cfe9d6ac672a3b
slomanl1 <slomanl75@gmail.com>
2020-05-30 21:12:08 -0600

    Remove writedate()

--- FixRuinedFdates.R
+++ FixRuinedFdates.R
@@ -8,10 +8,10 @@ gdn$bn=beaner(gdn$fnx)
 gdo$bn=beaner(gdo$fnx)
 mgg=merge(gdn,gdo,by='bn')
 mm=mgg[which(as.Date(mgg$Date.x) - as.Date(mgg$Date.y)>10),c('fnx.x','fnx.y','Date.x','Date.y')]
-if(nrow(mm)>0){
-  source('~/Pllist.Git/writeDate.R')
-  for( i in 1:nrow(mm)) WriteDate(mm$fnx.x[i],mm$Date.y[i])
-}
+# if(nrow(mm)>0){
+#   source('~/Pllist.Git/writeDate.R')
+#   for( i in 1:nrow(mm)) WriteDate(mm$fnx.x[i],mm$Date.y[i])
+# }
 
 gdf1=gfdo
 gdframe$fnn=sub('_New','',gdframe$fnx)
